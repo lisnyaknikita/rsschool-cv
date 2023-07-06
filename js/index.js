57bdd916c1d13680515ae3729cb1d39100e5f625
nlisnyak <lisnyaknikita@gmail.com>
2023-07-06 12:31:40 +0300

    feat: implement header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+function burger() {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+  const body = document.querySelector('body');
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    burger.classList.toggle('active-burger');
+    body.classList.toggle('locked');
+  });
+}
+
+burger();
